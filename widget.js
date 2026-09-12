a98830b4debddd0cbb7df5ca50d63708d98a7aa5
Aarav-B24 <aarav.bhatnagar@hotmail.com>
2026-09-12 15:46:39 +0930

    Fix formatting of chatContainer innerHTML assignment

--- widget.js
+++ widget.js
@@ -17,7 +17,7 @@
     chatContainer.style.justifyContent = 'center';
     chatContainer.style.zIndex = '999999';
     chatContainer.style.fontSize = '30px';
-    chatContainer.innerHTML = '🤖'; 
+   chatContainer.innerHTML = '🤖';
     
     // 2. Click interaction logic to launch the AI receptionist
     chatContainer.onclick = function() {
